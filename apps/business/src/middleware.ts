168acf77bb5eca9bba4ac759aa5753f327242000
Bhaskaran Rathnachalam <r.bhaskarann@gmail.com>
2025-01-03 11:40:11 -0800

    change middleware

--- apps/business/src/middleware.ts
+++ apps/business/src/middleware.ts
@@ -1,11 +1,92 @@
-import { NextResponse } from "next/server";
+import { updateSession } from "@loopearn/supabase/middleware";
+import { createClient } from "@loopearn/supabase/server";
+import type { JwtPayload } from "@loopearn/supabase/types";
+import { jwtDecode } from "jwt-decode";
+import { createI18nMiddleware } from "next-international/middleware";
+import { type NextRequest, NextResponse } from "next/server";
 
-export function middleware(request) {
-  if (request.nextUrl.pathname.startsWith("/protected")) {
-    // Redirect unauthenticated users to the login page
-    return NextResponse.redirect(new URL("/login", request.url));
+const I18nMiddleware = createI18nMiddleware({
+  locales: ["en"],
+  defaultLocale: "en",
+  urlMappingStrategy: "rewrite",
+});
+
+export async function middleware(request: NextRequest) {
+  const response = await updateSession(request, I18nMiddleware(request));
+  const supabase = createClient();
+  const url = new URL("/", request.url);
+  const nextUrl = request.nextUrl;
+
+  const pathnameLocale = nextUrl.pathname.split("/", 2)?.[1];
+
+  // Remove the locale from the pathname
+  const pathnameWithoutLocale = pathnameLocale
+    ? nextUrl.pathname.slice(pathnameLocale.length + 1)
+    : nextUrl.pathname;
+
+  // Create a new URL without the locale in the pathname
+  const newUrl = new URL(pathnameWithoutLocale || "/", request.url);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // Not authenticated
+  if (
+    !session &&
+    newUrl.pathname !== "/login" &&
+    newUrl.pathname !== "/signup" &&
+    !newUrl.pathname.includes("/report")
+  ) {
+    const encodedSearchParams = `${newUrl.pathname.substring(1)}${
+      newUrl.search
+    }`;
+
+    const url = new URL("/login", request.url);
+
+    if (encodedSearchParams) {
+      url.searchParams.append("return_to", encodedSearchParams);
+    }
+
+    return NextResponse.redirect(url);
   }
 
-  // Allow the request to proceed
-  return NextResponse.next();
+  // If authenticated but no full_name redirect to user setup page
+  if (
+    newUrl.pathname !== "/setup" &&
+    session &&
+    !session?.user?.user_metadata?.full_name
+  ) {
+    return NextResponse.redirect(`${url.origin}/setup`);
+  }
+
+  if (session?.access_token) {
+    const jwt = jwtDecode<JwtPayload>(session.access_token);
+
+    // if not a business account, redirect to the home page
+    if (!jwt.industry || jwt.industry !== "business") {
+      // sign out & redirect to login
+      await supabase.auth.signOut();
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  const { data: mfaData } =
+    await supabase.auth.mfa.getAuthenticatorAssuranceLevel();
+
+  // Enrolled for mfa but not verified
+  if (
+    mfaData &&
+    mfaData.nextLevel === "aal2" &&
+    mfaData.nextLevel !== mfaData.currentLevel &&
+    newUrl.pathname !== "/mfa/verify"
+  ) {
+    return NextResponse.redirect(`${url.origin}/mfa/verify`);
+  }
+
+  return response;
 }
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api|monitoring).*)"],
+};
