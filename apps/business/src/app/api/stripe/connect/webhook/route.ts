7a4ffa70cf2d8a570cd34d752c7881242915f89b
Bhaskaran Rathnachalam <r.bhaskarann@gmail.com>
2025-01-22 11:48:34 -0800

    change webhooksecret to connect

--- apps/business/src/app/api/stripe/connect/webhook/route.ts
+++ apps/business/src/app/api/stripe/connect/webhook/route.ts
@@ -9,7 +9,7 @@ const relevantEvents = new Set(["account.updated"]);
 export const POST = async (req: Request) => {
   const buf = await req.text();
   const sig = req.headers.get("Stripe-Signature") as string;
-  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  const webhookSecret = process.env.STRIPE_CONNECT_WEBHOOK_SECRET;
   let event: Stripe.Event;
   try {
     if (!sig || !webhookSecret) return;
